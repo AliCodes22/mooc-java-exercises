39d41170f39b65e136bcb42c8917e98c8b6254cf
Ali Ahmed <alitine94@gmail.com>
2024-04-24 03:23:04 -0400

    couple more exercises done

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,29 @@
-
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double sum=0;
+    double count=0;
+    
+    while(true){
+        double num = Double.valueOf(scanner.nextLine());
+        
+        if(num==0){
+            break;
+        }
+        if(num>0){
+            sum += num;
+            count++;
+        }
     }
+    if(count<=0){
+        System.out.println("Cannot calculate the average");
+    }
+    double avarage = sum/count;
+    if(avarage >0){
+        System.out.println(avarage);
+    }
+}
 }
