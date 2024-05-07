e0c784a9b895400d193126fc462c78832acfccdb
Ali Ahmed <alitine94@gmail.com>
2024-05-07 04:36:54 -0400

    2 exercises done

--- part03-Part03_04.ListSize/src/main/java/ListSize.java
+++ part03-Part03_04.ListSize/src/main/java/ListSize.java
@@ -7,15 +7,22 @@ public class ListSize {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
+        ArrayList<String> names = new ArrayList<>();
+
+        while(true) {
+            System.out.println("Enter something");
+            String something = String.valueOf(scanner.nextLine());
+
+            if(something.equals("")) {
                 break;
             }
 
-            list.add(input);
+            
+            names.add(something);
+           
         }
+
+        System.out.println("In total: " + names.size());
 
     }
 }
