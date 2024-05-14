e7fab6a878a1a2fb63f7f0ed8a2f800deee42936
Ali Ahmed <alitine94@gmail.com>
2024-05-14 03:42:32 -0400

    2 exercises done

--- part03-Part03_14.OnTheList/src/main/java/OnTheList.java
+++ part03-Part03_14.OnTheList/src/main/java/OnTheList.java
@@ -7,15 +7,29 @@ public class OnTheList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> list = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("")) {
-                break;
-            }
+       ArrayList<String> names = new ArrayList<>();
 
-            list.add(input);
+       while(true) {
+        String input = String.valueOf(scanner.nextLine());
+
+        if(input.equals("")) {
+            break;
         }
+
+        names.add(input);
+ 
+       }
+
+       System.out.println("Search for?");
+       String name = String.valueOf(scanner.nextLine());
+      
+       boolean isFound = names.contains(name);
+
+       if(isFound){
+        System.out.println(name + " was found!");
+       } else {
+        System.out.println(name + " was not found!");
+       }
 
     }
 }
