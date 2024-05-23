3920bd1024e0821e0ed4636ff6fee5a674314615
Ali Ahmed <alitine94@gmail.com>
2024-05-23 02:56:48 -0400

    part 4 oop

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -1,8 +1,14 @@
 
+
 public class YourFirstAccount {
 
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account alisAccount = new Account("Ali's account", 100.0);
+        alisAccount.deposit(20.0);
+
+        System.out.println(alisAccount);
+
     }
 }
