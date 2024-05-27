71760d48da2a15bfda06b7d7d793158f96cf3fa2
Ali Ahmed <alitine94@gmail.com>
2024-05-26 23:27:06 -0400

    exercises

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,16 @@
 
 public class Statistics {
+    private int count;
+
+    public Statistics() {
+        int numberCount = this.count;
+    } 
+
+    public void addNumber(int number) {
+        count++;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
 }
