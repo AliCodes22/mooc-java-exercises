0f9f1712a18be6906a9bc6d26badcd081dbbaa8c
Ali Ahmed <alitine94@gmail.com>
2024-05-30 01:45:50 -0400

    exercises

---
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,36 @@
+public class PaymentCard {
+    private double initialBalance;
+
+    public PaymentCard(double initialBalance) {
+        this.initialBalance = initialBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.initialBalance + " euros";
+    }
+
+    public void eatAffordably() {
+        if(this.initialBalance < 0) {
+            this.initialBalance = 0;
+        }
+
+        this.initialBalance -= 2.60;
+    }
+
+    public void eatHeartily() {
+        if(this.initialBalance < 0) {
+            this.initialBalance = 0;
+        }
+
+        this.initialBalance -= 4.60;
+    }
+
+    public void addMoney(double amount) {
+        if(amount < 0) {
+            this.initialBalance += 0;
+        }
+
+        this.initialBalance += amount;
+    }
+
+}
