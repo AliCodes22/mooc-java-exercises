701e0af0c27adec2dd5c201de41c278baed3d7c5
Ali Ahmed <alitine94@gmail.com>
2024-05-30 02:02:26 -0400

    exercise

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -6,6 +6,26 @@ public class MainProgram {
         
         // Here you can write code to test that PaymentCard works as intended
         // be sure to erase the extra code for the last part of the exercise!
+        PaymentCard paulsCard = new PaymentCard(20.0);
+        PaymentCard mattsCard = new PaymentCard(30.0);
 
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        paulsCard.toString();
+        mattsCard.toString();
+
+        paulsCard.addMoney(20.0);
+        mattsCard.eatHeartily();
+
+        paulsCard.toString();
+        mattsCard.toString();
+
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+
+        mattsCard.addMoney(50.0);
+
+        paulsCard.toString();
+        mattsCard.toString();
     }
 }
--- part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -10,24 +10,24 @@ public class PaymentCard {
     }
 
     public void eatAffordably() {
-        if(this.initialBalance < 0) {
-            this.initialBalance = 0;
+        if(this.initialBalance < 0.0) {
+            this.initialBalance = 0.0;
         }
 
         this.initialBalance -= 2.60;
     }
 
     public void eatHeartily() {
-        if(this.initialBalance < 0) {
-            this.initialBalance = 0;
+        if(this.initialBalance < 0.0) {
+            this.initialBalance = 0.0;
         }
 
         this.initialBalance -= 4.60;
     }
 
     public void addMoney(double amount) {
-        if(amount < 0) {
-            this.initialBalance += 0;
+        if(amount < 0.0) {
+            this.initialBalance += 0.0;
         }
 
         this.initialBalance += amount;
