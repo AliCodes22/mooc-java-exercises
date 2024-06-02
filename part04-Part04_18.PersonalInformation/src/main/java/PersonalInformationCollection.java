23119f49ba5efbeb22979fe444d7cb5f892ad1d1
Ali Ahmed <alitine94@gmail.com>
2024-06-02 06:06:35 -0400

    exercises

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,29 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            System.out.println("Enter first name");
+            String firstName = scanner.nextLine();
+
+            if(firstName.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Enter last name:");
+            String lastName = scanner.nextLine();
+
+            System.out.println("Enter id number");
+            String identificationNumber = scanner.nextLine();
+
+            PersonalInformation person = new PersonalInformation(firstName,lastName,identificationNumber);
+
+            infoCollection.add(person);
+
+        }
+
+        for(PersonalInformation person: infoCollection) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
+
     }
 }
