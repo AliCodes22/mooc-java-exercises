e70aa5c1f74403172c3385e3fc2a77d8c00c1b9d
Ali Ahmed <alitine94@gmail.com>
2024-06-08 05:04:53 -0400

    exercises

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,4 +1,5 @@
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -7,5 +8,21 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Which file should have its contents printed");
+        String input = scanner.nextLine();
+
+        try(Scanner reader = new Scanner(Paths.get(input))) {
+            while(reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+
+        }
+
+
     }
+
 }
