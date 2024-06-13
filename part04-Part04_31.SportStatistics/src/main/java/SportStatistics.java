5b0c16e29024b92b8bd8b4b8496e3ac6d99d800d
Ali Ahmed <alitine94@gmail.com>
2024-06-13 03:22:32 -0400

    exercise half done

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -8,6 +8,46 @@ public class SportStatistics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-    }
+        System.out.println("File:");
+        String fileName = scan.nextLine().toUpperCase();
+        System.out.println("Team:");
+        String teamName = scan.nextLine();
 
+        int gamesPlayed = 0;
+        int wins = 0;
+        int losses = 0;
+
+
+        try(Scanner reader = new Scanner(Paths.get(fileName))) {
+            while(reader.hasNextLine()) {
+                String row = reader.nextLine();
+                String parts[] = row.split(",");
+                int homeTeamPoints = Integer.valueOf(parts[2]);
+                int visitingTeamPoints = Integer.valueOf(parts[3]);
+                
+                Boolean isHomeTeam = parts[0].equals(teamName);
+                Boolean isVisitingTeam = parts[1].equals(teamName);
+
+
+                if(parts[0].equals(teamName) || parts[1].equals(teamName)) {
+                    gamesPlayed++;
+                }
+
+              if(isHomeTeam && homeTeamPoints > visitingTeamPoints) {
+    wins++;
+} else if (isVisitingTeam && visitingTeamPoints > homeTeamPoints) {
+    wins++;
+} else {
+    losses++;
+}
+
+            }
+
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
+        System.out.println("Games: " + gamesPlayed);
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses );
+    }
 }
