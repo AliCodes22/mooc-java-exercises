d404700f442836aee246f429e87343f02fca73af
Ali Ahmed <alitine94@gmail.com>
2024-06-23 21:40:31 -0400

    exercises

--- part05-Part05_10.CardPayments/src/main/java/Main.java
+++ part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -3,6 +3,19 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental main programs here
+
+        PaymentTerminal cafeTerminal = new PaymentTerminal();
+
+        double change = cafeTerminal.eatAffordably(10);
+        System.out.println("remaining change " + change);
+
+        change = cafeTerminal.eatAffordably(5);
+        System.out.println("remaining change " +  change);
+
+        change = cafeTerminal.eatHeartily(4.3);
+        System.out.println("remaining change " + change );
+
+        System.out.println(cafeTerminal);
     }
 }
 
--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -10,6 +10,8 @@ public class PaymentTerminal {
     public PaymentTerminal() {
         // register initially has 1000 euros of money
         this.money = 1000;
+        this.heartyMeals = 0;
+        this.affordableMeals = 0;
 
     }
 
@@ -17,12 +19,16 @@ public class PaymentTerminal {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+        double change = 0;
 
-        if(payment == 2.50) {
+        if(payment >= 2.50) {
+            change = payment - 2.50;
             this.money += 2.50;
+            this.affordableMeals++;
+            return change;
+        } else {
+            return payment;
         }
-        
-        return this.money - 2.50;
 
        
     }
@@ -31,8 +37,49 @@ public class PaymentTerminal {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+
+        if(payment >= 4.30) {
+           
+            double change = payment - 4.30;
+             this.money += 4.30;
+            this.heartyMeals++;
+
+            return change;
+        } else {
+            return payment;
+        }
     }
+
+    public boolean eatAffordably(PaymentCard card) {
+        if(card.balance() >= 2.50) {
+           
+        this.affordableMeals++;
+        
+        return card.takeMoney(2.50);
+           
+        } else {
+            return false;
+        }
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        if(card.balance() >= 4.30) {
+            
+            this.heartyMeals++;
+            return card.takeMoney(4.30);
+            
+        } else {
+            
+            return false;
+        }
+    }
+     public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum > 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
+    }
+
 
 
     @Override
