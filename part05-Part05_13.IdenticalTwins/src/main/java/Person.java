e0ad387584c98661f6b33d0bbd948a485beedbff
Ali Ahmed <alitine94@gmail.com>
2024-06-28 03:32:21 -0400

    exercises

--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -15,5 +15,26 @@ public class Person {
         this.weight = weight;
     }
 
-    // implement an equals method here for checking the equality of objects
+    // implement an equals method here for checking the equality of object
+    
+    @Override 
+    public boolean equals(Object compared) {
+        if(this == compared) {
+            return true;
+        } 
+
+        if(!(compared instanceof Person)) {
+            return false;
+        }
+
+        Person otherPerson = (Person) compared;
+      
+
+        if(otherPerson.name.equals(this.name) && otherPerson.birthday.equals(this.birthday) && otherPerson.height == this.height && otherPerson.weight == this.weight) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
--- part05-Part05_14.Books/src/main/java/Book.java
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,23 @@ public class Book {
         return publicationYear;
     }
 
+    @Override 
+    public boolean equals(Object compared) {
+        if(this == compared) {
+            return true;
+        }
+
+        if(!(compared instanceof Book)) {
+            return false;
+        }
+        
+        Book otherBook = (Book) compared;
+
+        if(otherBook.name.equals(this.name) && otherBook.publicationYear == this.publicationYear) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
