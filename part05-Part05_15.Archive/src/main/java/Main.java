533fa0d3a64e75e6900b4c56e3bf1aef533c95fc
Ali Ahmed <alitine94@gmail.com>
2024-06-29 04:53:00 -0400

    exercises

--- part05-Part05_15.Archive/src/main/java/Main.java
+++ part05-Part05_15.Archive/src/main/java/Main.java
@@ -6,7 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList <Item> items = new ArrayList<>();
 
+        while(true) {
+            System.out.println("Identifier?");
+            String identifier = scanner.nextLine();
 
-    }
+            if(identifier.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Name?");
+            String name = scanner.nextLine();
+
+            if(name.isEmpty()) {
+                break;
+            }
+
+            Item item = new Item(name, identifier);
+            
+            if(items.contains(item)) {
+                continue;
+            } else {
+                items.add(item);
+            }
+        }
+
+        for(Item item : items) {
+            System.out.println(item.toString());
+        }
+     }
 }
