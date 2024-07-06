8ea1691a12e8c3831a078e7665d3ef0af0c98621
Ali Ahmed <alitine94@gmail.com>
2024-07-06 15:37:16 -0400

    exercises

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -1,6 +1,8 @@
 
 import java.util.ArrayList;
 
+import javax.xml.crypto.dsig.SignatureMethod;
+
 public class SimpleCollection {
 
     private String name;
@@ -17,6 +19,23 @@ public class SimpleCollection {
 
     public ArrayList<String> getElements() {
         return this.elements;
+    }
+
+    @Override
+    public String toString() {
+        if(this.elements.isEmpty()) {
+           return "The collection " + this.name + " is empty.";
+        } else if(this.elements.size() == 1) {
+            return "The collection " + this.name + " has 1 " + "element:" + this.elements;
+        } else {
+            return "The collection " + "this.name" + " has " + this.elements.size() + " elements."; 
+            for(String element: this.elements) {
+            return element;
+         }
+
+        }
+
+       
     }
     
 }
