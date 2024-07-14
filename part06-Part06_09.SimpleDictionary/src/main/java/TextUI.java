50cf3aabdfbe06cdbcfd6f8420a5b7fcde096137
Ali Ahmed <alitine94@gmail.com>
2024-07-14 04:17:57 -0400

    exercises

--- part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -18,7 +18,26 @@ public class TextUI {
             if(input.equals("end")) {
                 System.out.println("Bye bye!");
                 break;
-            } else {
+            } else if(input.equals("add")) {
+                System.out.println("Word?");
+                String word = scanner.nextLine();
+                System.out.println("Translation?");
+                String translation = scanner.nextLine();    
+                dictionary.add(word, translation);            
+            }else if (input.equals("search")) {
+                System.out.println("Word to translate:");
+                String wordToTranslate = scanner.nextLine();
+
+                String translation = dictionary.translate(wordToTranslate);
+
+                if (translation != null) {
+                    System.out.println("Translation: " + translation);
+                } else {
+                    System.out.println("Word " + wordToTranslate + " was not found");
+                }
+            }
+            
+            else {
                 System.out.println("Unknown command");
             }
         }
