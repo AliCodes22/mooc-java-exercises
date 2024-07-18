797d112b4c0cb1043675cff76a98d1e1473b0741
Ali Ahmed <alitine94@gmail.com>
2024-07-18 02:53:05 -0400

    exercises

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,6 +5,9 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String first = "0/100";
+        String second = "0/100";
+
 
 
         while (true) {
@@ -13,6 +16,13 @@ public class LiquidContainers {
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            } else if (input.equals("add")) {
+                String number = scan.nextLine();
+
+                int numberToAdd = Integer.valueOf(number);
+                int firstPart = Integer.valueOf(first.split("/")[0]);
+                firstPart += numberToAdd;
+
             }
 
         }
